5814fdff18e286bf44514b1bf4372bf2ac7c9d26
Rahul <Rahul@DESKTOP-7HRJIEC>
2018-03-31 22:06:04 +0530

    Changes in code to pass TC 6

--- TechnicalTestCore/HelloWorld.cs
+++ TechnicalTestCore/HelloWorld.cs
@@ -1,11 +1,15 @@
 ﻿namespace TechnicalTestCore
+    
 {
     using System;
-
+    
     internal class HelloWorld
     {
-        public static void SayHelloToTheWorld(string[] args)
+
+        private static readonly string Newline = Environment.NewLine;
+        public static void SayHelloToTheWorld(String[] args)
         {
+            Console.WriteLine(("Hello World!"));
             // Write a method body which makes the corresponding unit test in the 'TechnicalTestUnitTests' project pass
         }
     }
