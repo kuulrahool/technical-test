e351fce2fafd99e910692899d347b24f2d74631a
Rahul <Rahul@DESKTOP-7HRJIEC>
2018-04-01 11:51:16 +0530

    making the TC pass by using System.Linq

--- TechnicalTestCore/Minimum.cs
+++ TechnicalTestCore/Minimum.cs
@@ -1,13 +1,15 @@
 ﻿namespace TechnicalTestCore
 {
     using System.Collections.Generic;
+    using System.Linq;
 
     internal class Minimum
     {
         public static int MinimumElementIn(List<int> intlist)
         {
             // Adapt the method body to make the corresponding unit test in 'TechnicalTestUnitTests' pass
-            return 1533;
+            int result = intlist.Min();
+            return result;
         }
     }
 }
